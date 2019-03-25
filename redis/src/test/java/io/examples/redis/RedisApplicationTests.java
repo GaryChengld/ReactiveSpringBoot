e0f623ab1fabfd8816b89961e2efa4672acf8b0f
GaryChengld <chengld@yahoo.com>
2019-03-24 22:33:10 -0400

    unit tests

--- redis/src/test/java/io/examples/redis/RedisApplicationTests.java
+++ redis/src/test/java/io/examples/redis/RedisApplicationTests.java
@@ -1,15 +1,35 @@
 package io.examples.redis;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisApplicationTests {
+    @Autowired
+    private ApplicationContext context;
+    private WebTestClient client;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Before
+    public void setup() {
+        client = WebTestClient
+                .bindToApplicationContext(context)
+                .configureClient()
+                .baseUrl("http://localhost:8080/")
+                .build();
+    }
+
+    @Test
+    public void getAll() {
+        client.get()
+                .uri("/v1/product/")
+                .exchange()
+                .expectStatus().isOk();
+    }
 }
