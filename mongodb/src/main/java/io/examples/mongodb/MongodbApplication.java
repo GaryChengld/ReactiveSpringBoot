e04ca544da194c38d604aed5781ceb9f528ca8ab
GaryChengld <chengld@yahoo.com>
2019-04-01 21:59:56 -0400

    mongodb authentication database

--- mongodb/src/main/java/io/examples/mongodb/MongodbApplication.java
+++ mongodb/src/main/java/io/examples/mongodb/MongodbApplication.java
@@ -1,16 +1,29 @@
 package io.examples.mongodb;
 
+import io.examples.mongodb.handler.BlogHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
 @EnableMongoAuditing
 public class MongodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routes(
+            BlogHandler blogHandler) {
+        return nest(path("/v1/blogs"), blogHandler.getRouterFunction());
+    }
 }
---
+++ mongodb/src/main/java/io/examples/mongodb/handler/BlogHandler.java
@@ -0,0 +1,88 @@
+package io.examples.mongodb.handler;
+
+import io.examples.mongodb.domain.Blog;
+import io.examples.mongodb.repository.BlogRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Component
+@Slf4j
+public class BlogHandler {
+    private final BlogRepository blogRepository;
+
+    public BlogHandler(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    public RouterFunction<ServerResponse> getRouterFunction() {
+        return nest(accept(APPLICATION_JSON),
+                route(GET("/"), this::all)
+                        .andRoute(GET("/{id}"), this::byId)
+                        .andRoute(POST("/"), this::create)
+                        .andRoute(PUT("/{id}"), this::update)
+                        .andRoute(DELETE("/{id}"), this::delete)
+        );
+    }
+
+    private Mono<ServerResponse> all(ServerRequest request) {
+        log.debug("Received find all blogs request");
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(this.blogRepository.findAll(), Blog.class);
+    }
+
+    private Mono<ServerResponse> byId(ServerRequest request) {
+        log.debug("Received find blog by id request");
+        return this.blogRepository.findById(request.pathVariable("id"))
+                .flatMap(this::buildResponse)
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    private Mono<ServerResponse> create(ServerRequest request) {
+        log.debug("Received create blog request");
+        return request.bodyToMono(Blog.class)
+                .flatMap(this.blogRepository::save)
+                .flatMap(this::buildResponse);
+    }
+
+    private Mono<ServerResponse> update(ServerRequest request) {
+        log.debug("Received update blog request");
+        AtomicReference<Blog> blogRef = new AtomicReference<>();
+        return request.bodyToMono(Blog.class)
+                .doOnNext(blogRef::set)
+                .flatMap(b -> this.blogRepository.findById(request.pathVariable("id")))
+                .map(b -> {
+                    Blog blog = blogRef.get();
+                    b.setTitle(blog.getTitle());
+                    b.setContent(blog.getContent());
+                    b.setAuthor(blog.getAuthor());
+                    return b;
+                })
+                .flatMap(this.blogRepository::save)
+                .flatMap(this::buildResponse)
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    private Mono<ServerResponse> delete(ServerRequest request) {
+        log.debug("Received delete blog request");
+        return ServerResponse.noContent().build(this.blogRepository.deleteById(request.pathVariable("id")));
+    }
+
+    private <T> Mono<ServerResponse> buildResponse(T body) {
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(fromObject(body));
+    }
+}
