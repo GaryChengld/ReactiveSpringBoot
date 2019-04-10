85bec13c46d569d5a68c1ba69552a4e51d958305
GaryChengld <chengld@yahoo.com>
2019-04-09 22:12:49 -0400

    RxJava2 style mongo app

--- rxjava-mongodb/src/main/java/io/examples/mongodb/controller/RxJava2BlogController.java
+++ rxjava-mongodb/src/main/java/io/examples/mongodb/controller/RxJava2BlogController.java
@@ -29,11 +29,12 @@ public class RxJava2BlogController {
     }
 
     @GetMapping(value = "/{id}")
-    public Maybe<ResponseEntity<?>> byId(@PathVariable("id") String id) {
+    public Single<ResponseEntity<?>> byId(@PathVariable("id") String id) {
         log.debug("Received byId() request, id:{}", id);
         return blogRepository.findById(id)
                 .<ResponseEntity<?>>map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .toSingle();
     }
 
     @GetMapping(value = "/byAuthor/{author}")
@@ -55,16 +56,17 @@ public class RxJava2BlogController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public Maybe<ResponseEntity> delete(@PathVariable("id") String id) {
+    public Single<ResponseEntity> delete(@PathVariable("id") String id) {
         log.debug("Received delete() request, id:{}", id);
         return blogRepository.findById(id)
                 .flatMap(this::deleteBlog)
                 .<ResponseEntity>map(b -> ResponseEntity.noContent().build())
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .toSingle();
     }
 
     @PutMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE)
-    public Maybe<ResponseEntity<?>> update(@PathVariable("id") String id, @RequestBody Blog blog) {
+    public Single<ResponseEntity<?>> update(@PathVariable("id") String id, @RequestBody Blog blog) {
         log.debug("Received update() request, blog:{}", blog);
         return blogRepository.findById(id)
                 .flatMap(b -> {
@@ -74,11 +76,12 @@ public class RxJava2BlogController {
                     return this.blogRepository.save(b).toMaybe();
                 })
                 .<ResponseEntity<?>>map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .toSingle();
     }
 
     public Maybe<Boolean> deleteBlog(Blog blog) {
         return Maybe.create(emitter ->
-                this.blogRepository.delete(blog).subscribe(() -> emitter.onSuccess(true), emitter::onError));
+                this.blogRepository.delete(blog).subscribe(() -> emitter.onSuccess(Boolean.TRUE), emitter::onError));
     }
 }
