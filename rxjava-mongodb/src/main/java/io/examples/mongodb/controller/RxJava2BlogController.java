0f5d29180284efd5df27504926ac20c97c2ff5ee
GaryChengld <chengld@yahoo.com>
2019-04-09 21:45:33 -0400

    RxJava2 style mongo app

--- rxjava-mongodb/src/main/java/io/examples/mongodb/controller/RxJava2BlogController.java
+++ rxjava-mongodb/src/main/java/io/examples/mongodb/controller/RxJava2BlogController.java
@@ -4,13 +4,13 @@ import io.examples.mongodb.domain.Blog;
 import io.examples.mongodb.repository.BlogRepository;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
+import io.reactivex.Single;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * @author Gary Cheng
@@ -34,5 +34,51 @@ public class RxJava2BlogController {
         return blogRepository.findById(id)
                 .<ResponseEntity<?>>map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(value = "/byAuthor/{author}")
+    public Flowable<Blog> byAuthor(@PathVariable("author") String author) {
+        log.debug("Received byAuthor() request, author:{}", author);
+        return this.blogRepository.findByAuthor(author);
+    }
+
+    @GetMapping(value = "/search/{text}")
+    public Flowable<Blog> search(@PathVariable("text") String text) {
+        log.debug("Received search() request, text:{}", text);
+        return this.blogRepository.searchByKeyword(text);
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public Single<Blog> create(@RequestBody Blog blog) {
+        log.debug("Received create() request, blog:{}", blog);
+        return this.blogRepository.save(blog);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public Maybe<ResponseEntity> delete(@PathVariable("id") String id) {
+        log.debug("Received delete() request, id:{}", id);
+        return blogRepository.findById(id)
+                .flatMap(this::deleteBlog)
+                .<ResponseEntity>map(b -> ResponseEntity.noContent().build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE)
+    public Maybe<ResponseEntity<?>> update(@PathVariable("id") String id, @RequestBody Blog blog) {
+        log.debug("Received update() request, blog:{}", blog);
+        return blogRepository.findById(id)
+                .flatMap(b -> {
+                    b.setTitle(blog.getTitle());
+                    b.setContent(blog.getContent());
+                    b.setAuthor(blog.getAuthor());
+                    return this.blogRepository.save(b).toMaybe();
+                })
+                .<ResponseEntity<?>>map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    public Maybe<Boolean> deleteBlog(Blog blog) {
+        return Maybe.create(emitter ->
+                this.blogRepository.delete(blog).subscribe(() -> emitter.onSuccess(true), emitter::onError));
     }
 }
