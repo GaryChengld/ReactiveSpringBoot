bf091737185ae15077026bac845b8c4b956e36de
GaryChengld <chengld@yahoo.com>
2019-04-22 20:52:43 -0400

    Security

--- redis-mongodb/src/main/java/io/examples/blog/BlogApp.java
+++ redis-mongodb/src/main/java/io/examples/blog/BlogApp.java
@@ -1,9 +1,24 @@
 package io.examples.blog;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.examples.blog.domain.Blog;
+import io.examples.blog.handler.BlogHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
@@ -12,4 +27,22 @@ public class BlogApp {
     public static void main(String[] args) {
         SpringApplication.run(BlogApp.class, args);
     }
+
+    @Bean
+    ReactiveRedisOperations<String, Blog> redisOperations(ReactiveRedisConnectionFactory factory, ObjectMapper mapper) {
+        Jackson2JsonRedisSerializer<Blog> serializer = new Jackson2JsonRedisSerializer<>(Blog.class);
+        serializer.setObjectMapper(mapper);
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Blog> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+        RedisSerializationContext<String, Blog> context = builder.value(serializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routes(
+            BlogHandler blogHandler) {
+        return nest(path("/v1/blogs"), blogHandler.getRouterFunction());
+    }
+
+
 }
---
+++ redis-mongodb/src/main/java/io/examples/blog/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package io.examples.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author Gary Cheng
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+        return http
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.GET, "/v1/blogs/**").permitAll()
+                .pathMatchers(HttpMethod.DELETE, "/v1/blogs/**").hasRole("ADMIN")
+                .pathMatchers("/v1/blogs/**").authenticated()
+                .anyExchange().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsRepository() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("password").roles("USER", "ADMIN").build();
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+
+}
