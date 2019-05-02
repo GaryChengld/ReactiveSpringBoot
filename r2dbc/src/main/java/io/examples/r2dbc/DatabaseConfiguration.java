73a369a2a56d87e118ad15cc1ab3d8ce22dc5404
GaryChengld <chengld@yahoo.com>
2019-05-01 20:24:04 -0400

    R2DBC

--- r2dbc/src/main/java/io/examples/r2dbc/DatabaseConfiguration.java
+++ r2dbc/src/main/java/io/examples/r2dbc/DatabaseConfiguration.java
@@ -3,6 +3,7 @@ package io.examples.r2dbc;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
@@ -14,14 +15,26 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 @Configuration
 @EnableR2dbcRepositories
 public class DatabaseConfiguration extends AbstractR2dbcConfiguration {
+    @Value("${spring.data.postgres.host}")
+    private String host;
+    @Value("${spring.data.postgres.port}")
+    private Integer port;
+    @Value("${spring.data.postgres.database}")
+    private String database;
+    @Value("${spring.data.postgres.username}")
+    private String username;
+    @Value("${spring.data.postgres.password}")
+    private String password;
+
     @Override
     public ConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(
                 PostgresqlConnectionConfiguration.builder()
-                        .host("localhost")
-                        .database("myDB")
-                        .username("postgres")
-                        .password("manager").build()
+                        .host(host)
+                        .port(port)
+                        .database(database)
+                        .username(username)
+                        .password(password).build()
         );
     }
 }
